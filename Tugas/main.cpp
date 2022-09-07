f5e3610684f1911c6b20a4ef67eff444ef1352e8
MDimasSutakuSumah <113001498+MDimasSutakuSumah@users.noreply.github.com>
2022-09-07 21:02:12 +0700

    Add files via upload
    File Nama Tugas: main.cpp

---
+++ Tugas/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system("color 2");
+    //PEREKNALAN
+    cout << "Nama:M.DIMAS SUTAKUSUMAH" << endl;
+    cout << "NIM:A11.2022.14617" << endl;
+    cout << "Alamat:Binagriya Pekalongan" << endl;
+    cout << "Hobi:Playing a Game" << endl;
+    return 0;
+}
