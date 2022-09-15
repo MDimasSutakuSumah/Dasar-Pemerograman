7494a0b27a3b5ef08fd88d6e1145fce0b74054d7
MDimasSutakuSumah <113001498+MDimasSutakuSumah@users.noreply.github.com>
2022-09-15 11:27:06 +0700

    Add files via upload
    Tugas Pertemuan 3

---
+++ Tugas DASPRO Pertemuan 3/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    /* Nama:M.Dimas SutakuSumah
+    NIM:A11.2022.14617
+    KELOMPOK 1146 */
+
+    cout << "Hello world!" << endl;
+
+    int u=20;
+    cout << "Umur Saya" << u;
+    cout << endl;
+
+    float t=170.1;
+    cout << "Tinggi Badan Saya" << t;
+    cout << endl;
+
+    string ubs="XS";
+    cout << "Ukuran Baju Saya" << ubs;
+    cout << endl;
+
+    int a;
+    cout << "\Masukan Angka";
+    cin >> a;
+    cout << "20" << a+u;
+    return 0;
+}
